74e6f21f243ba0031f9ebdb7b11266c896fd51de
Royalplayz123 <royalplayzpro@gmail.com>
2025-05-18 23:08:46 +0530

    Update install_skyport.sh

--- install_skyport.sh
+++ install_skyport.sh
@@ -2,10 +2,11 @@
 
 # ASCII Art
 ascii_art="
+
       _____                   _   _____  _                 
  |  __ \                 | | |  __ \| |                
  | |__) |___  _   _  __ _| | | |__) | | __ _ _   _ ____
- |  _  // _ \| | | |/ _\` | | |  ___/| |/ _\` | | | |_  /
+ |  _  // _ \| | | |/ _` | | |  ___/| |/ _` | | | |_  /
  | | \ \ (_) | |_| | (_| | | | |    | | (_| | |_| |/ / 
  |_|  \_\___/ \__, |\__,_|_| |_|    |_|\__,_|\__, /___|
                __/ |                          __/ |    
@@ -19,9 +20,12 @@ CYAN='\033[0;36m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
+echo_message() {
+  echo -e "${CYAN}$1${NC}"
+}
+
 # Clear the screen
 clear
-
 # Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
   echo -e "${RED}Please run this script as root.${NC}"
@@ -30,7 +34,7 @@ fi
 
 echo -e "${CYAN}$ascii_art${NC}"
 
-echo -e "${YELLOW}* Installing Dependencies${NC}"
+echo_message "* Installing Dependencies"
 
 # Update package list and install dependencies
 sudo apt update
@@ -39,32 +43,32 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install nodejs -y 
 sudo apt install git -y
 
-echo -e "${GREEN}* Installed Dependencies${NC}"
+echo_message "* Installed Dependencies"
 
-echo -e "${YELLOW}* Installing Files${NC}"
+echo_message "* Installing Files"
 
 # Create directory, clone repository, and install files
 mkdir -p panel5
-cd panel5 || { echo -e "${RED}Failed to change directory to panel5${NC}"; exit 1; }
+cd panel5 || { echo_message "Failed to change directory to skyport"; exit 1; }
 git clone https://github.com/achul123/panel5.git
-cd panel5 || { echo -e "${RED}Failed to change directory to panel${NC}"; exit 1; }
+cd panel5 || { echo_message "Failed to change directory to panel"; exit 1; }
 npm install
 
-echo -e "${GREEN}* Installed Files${NC}"
+echo_message "* Installed Files"
 
-echo -e "${YELLOW}* Starting Skyport${NC}"
+echo_message "* Starting Skyport"
 
 # Run setup scripts
 npm run seed
 npm run createUser
 
-echo -e "${YELLOW}* Starting Skyport With PM2${NC}"
+echo_message "* Starting Skyport With PM2"
 
 # Install panel and start the application
 sudo npm install -g pm2
 pm2 start index.js
 
-echo -e "${GREEN}* Skyport Installed and Started on Port 3001${NC}"
+echo_message "* Skyport Installed and Started on Port 3001"
 
 # Clear the screen after finishing
 clear
